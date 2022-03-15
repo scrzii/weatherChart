f3e5aab22ff0f1502483bdcb750db2a152525dba
Scrzii <mysyk.kz@ya.ru>
2022-03-15 20:43:45 +0400

    Нерабочий коммит

--- WeatherChart/Parser.cs
+++ WeatherChart/Parser.cs
@@ -7,44 +7,11 @@ using System.Threading.Tasks;
 
 namespace WeatherChart
 {
-    #region Json entities
-
-    internal class JsonResult
-    {
-        [JsonProperty("range_weather")]
-        public OneDay[] RangeWeather { get; set; }
-    }
-
-    internal class OneDay
-    {
-        [JsonProperty("forecast")]
-        public Dictionary<string, DayPart> Forecast { get; set; }
-    }
-
-    internal class DayPart
-    {
-        [JsonProperty("temperature")]
-        public int Temperature { get; set; }
-        [JsonProperty("wind_speed")]
-        public int WindSpeed { get; set; }
-        [JsonProperty("pressure")]
-        public int Pressure { get; set; }
-    }
-
-    #endregion
-
     internal class Parser
     {
         public static IEnumerable<int> Parse(string data)
         {
-            var result = JsonConvert.DeserializeObject<JsonResult>(data);
-            foreach (var day in result.RangeWeather)
-            {
-                foreach (var dayPart in day.Forecast)
-                {
-                    yield return dayPart.Value.Temperature;
-                }
-            }
+            return new List<int> { -5, 10, -18, 28, -16, 6, 1, -30, 6, 4 };
         }
     }
 }
